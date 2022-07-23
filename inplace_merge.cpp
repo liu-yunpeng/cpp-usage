12e661efebd48481da900c2c01c187d11b971db1
liu-yunpeng <liux3826@umn.edu>
2022-07-23 16:39:48 -0500

    Create inplace_merge.cpp

---
+++ inplace_merge.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    vector<int> sum({1, 2, 5, 8, 9, 13, 140, 2, 4, 5, 15, 16, 17});
+    int low = 0, mid = 7, high = 13; // high has to go over
+    //Merges two consecutive sorted ranges [first, middle) and [middle, last) into one sorted range [first, last)
+
+    inplace_merge(sum.begin() + low, sum.begin() + mid, sum.begin() + high);
+
+    for (int i : sum) cout << i << endl;
+
+    return 0;
+}
