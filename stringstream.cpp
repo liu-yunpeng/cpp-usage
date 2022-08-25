f8ece154a407a89d3e99e9c7360f61675c7897fe
liu-yunpeng <liux3826@umn.edu>
2022-08-24 22:01:20 -0500

    Create stringstream.cpp

---
+++ stringstream.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+#include <list>
+#include <stack>
+// using namespace std;std::
+
+
+
+
+int main()
+{
+    // cout<<"Hello World";
+    
+    std::stringstream ss("asdawdw:kkkkkk:asdasdasd");
+    
+    std::string temp, temp2, temp3;
+            std::getline(ss, temp, ':');
+            std::getline(ss, temp2, ':');
+            std::getline(ss, temp3, ':');
+
+    std::cout<<temp<<std::endl;;
+    std::cout<<temp2<<std::endl;
+    std::cout<<temp3;
+    return 0;
+}
+
+
